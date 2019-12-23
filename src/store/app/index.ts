fe2a4b6492773b602089f4b811a8877997d2a3f4
LLAP <ospiper@foxmail.com>
2019-12-23 13:24:26 +0800

    use class-styled vuex modules

---
+++ src/store/app/index.ts
@@ -0,0 +1,24 @@
+import { createModule } from 'vuex-class-component';
+
+const VuexModule = createModule({
+  namespaced: 'app',
+});
+
+export class AppStore extends VuexModule {
+  /**
+   * Version number
+   * Made up of a 32-bit unsigned number.
+   * 25 - 32 bits: Main version
+   * 17 - 24 bits: Sub version
+   * 9 - 16 bits: Feature version
+   * 1 - 8 bits: Patch version
+   */
+  private v: number = 0x00000001;
+
+  get version(): string {
+    return (this.v >> 24 & 0xFF) + '.' +
+      (this.v >> 16 & 0xFF) + '.' +
+      (this.v >> 8 & 0xFF) + '.' +
+      (this.v & 0xFF);
+  }
+}
--- src/store/index.ts
+++ src/store/index.ts
@@ -1,15 +1,16 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import { extractVuexModule, createProxy } from 'vuex-class-component';
+import { AppStore } from './app';
 
 Vue.use(Vuex)
 
-export default new Vuex.Store({
-  state: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
+export const store = new Vuex.Store({
   modules: {
+    ...extractVuexModule(AppStore)
   }
-})
+});
+
+export const vxm = {
+  app: createProxy(store, AppStore)
+}
