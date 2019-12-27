3b390db9e3ce7020f321e58d4385a1844ea0ae02
LLAP <ospiper@foxmail.com>
2019-12-27 23:16:57 +0800

    add org page (empty)

---
+++ src/views/org/org.vue
@@ -0,0 +1,14 @@
+<template>
+  <v-container>
+
+  </v-container>
+</template>
+<script>
+import Component from 'vue-class-component';
+import Vue from 'vue';
+
+@Component
+export default class Org extends Vue {
+
+}
+</script>
